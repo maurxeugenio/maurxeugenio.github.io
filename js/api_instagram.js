f6fe97b4c3e2f6e86a84b3896fbf4cddd847d021
maurx <maurx.dev@gmail.com>
2018-02-26 13:07:33 -0300

    21 years

--- js/api_instagram.js
+++ js/api_instagram.js
@@ -23,3 +23,25 @@ $.ajax({
 		console.log(data); // send the error notifications to console
 	}
 });
+
+
+function idade(ano_aniversario, mes_aniversario, dia_aniversario) {
+    var d = new Date,
+        ano_atual = d.getFullYear(),
+        mes_atual = d.getMonth() + 1,
+        dia_atual = d.getDate(),
+
+        ano_aniversario = +ano_aniversario,
+        mes_aniversario = +mes_aniversario,
+        dia_aniversario = +dia_aniversario,
+
+        quantos_anos = ano_atual - ano_aniversario;
+
+    if (mes_atual < mes_aniversario || mes_atual == mes_aniversario && dia_atual < dia_aniversario) {
+        quantos_anos--;
+    }
+
+    return quantos_anos < 0 ? 0 : quantos_anos;
+}
+
+console.log(idade(1997, 02, 26))
